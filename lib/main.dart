5bb70fe703764aa269857d53f1cb7cfc4da25620
Waleed Bensumaidea <waleed.com145@gmail.com>
2023-11-09 14:15:13 +0300

    Design MaterialApp => Scaffold =>  AppBar , BackgroundColor , Body

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const 
-  MaterialApp(
-    home: Center(
-      child: Text("Hello World"),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('I am Rich Man'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: const Center(
+          child: Image(
+            image:  NetworkImage(
+                'https://th.bing.com/th/id/OIP.avb9nDfw3kq7NOoP0grM4wHaEK?pid=ImgDet&rs=1'),
+          ),
+        ),
+      ),
     ),
-  ));
+  );
 }
