760f1c9ee0585b428c8e56c7e6002ecf5b119789
Waleed Bensumaidea <waleed.com145@gmail.com>
2023-11-14 08:36:23 +0300

    Challenge I am Poor App

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
+// I am Poor App Callenge
+
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('I am Rich Man'),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        backgroundColor: Colors.blueGrey,
-        body: const Center(
-          child: Image(image: AssetImage('Images/diamond.png')),
-        ),
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text("I am Poor App"),
+        backgroundColor: Colors.blue[200],
       ),
+      body: const Center(
+        child:  Image(image: AssetImage('Images/OIP.jpeg')),
+      ),
+      backgroundColor: Color.fromARGB(255, 45, 48, 52),
     ),
-  );
+  ));
 }
